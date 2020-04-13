038a86b1f22b73217bb7b83c4407a39aebd946ec
joook <outlook.s@163.com>
2020-04-13 11:09:40 +0800

    liushen: add practice 13.48

---
+++ 13.48/main.cpp
@@ -0,0 +1,34 @@
+#include "string_class.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printVecInfo(const vector<Str> &Vec)
+{
+    cout << "Vector size: " << Vec.size();
+    cout << ", capacity: " << Vec.capacity() << endl;
+}
+
+int main()
+{
+    vector<Str> VecStr;
+    Str Str1("abcd");
+    Str Str2("efgh");
+
+    //if the vector has not reserved enough space for elements,
+    //elements will be moved when the vector expands
+    VecStr.reserve(10);
+    printVecInfo(VecStr);
+
+    VecStr.push_back(Str1);
+    printVecInfo(VecStr);
+    VecStr.push_back(std::move(Str2));
+    printVecInfo(VecStr);
+    VecStr.push_back("ijkl");
+    printVecInfo(VecStr);
+    VecStr.push_back("ijkl");
+    printVecInfo(VecStr);
+
+    return 0;
+}
---
+++ 13.48/string_class.cpp
@@ -0,0 +1,234 @@
+#include "string_class.h"
+#include <stdexcept>
+#include <iostream>
+
+using namespace std;
+
+allocator<char> Str::m_Allocator;
+
+Str::Str()
+: m_First(nullptr)
+, m_FirstFree(nullptr)
+, m_Last(nullptr)
+{
+    reallocateMemory();
+}
+
+Str::Str(const char *StrPtr)
+{
+    /*
+    char *Ptr = StrPtr;
+    while((Ptr != nullptr) && (*Ptr != '\0'))
+    {
+        pushBack(*Ptr);
+    }
+    */
+
+    if(StrPtr != nullptr)
+    {
+        char *EndPtr = const_cast<char *>(StrPtr);
+        while(*EndPtr != '\0')
+        {
+            EndPtr++;
+        }
+        auto NewMemory = copyMemory(StrPtr, EndPtr);
+        m_First = NewMemory.first;
+        m_FirstFree = NewMemory.second;
+        m_Last = NewMemory.second;
+    }
+    else
+    {
+    }
+}
+
+Str::Str(size_t Length, const char Char)
+{
+    for(size_t i = 0; i != Length; i++)
+    {
+        pushBack(Char);
+    }
+}
+
+Str::Str(const Str &OriStr)
+: m_First(nullptr)
+, m_FirstFree(nullptr)
+, m_Last(nullptr)
+{
+    auto Pair = copyMemory(OriStr.m_First, OriStr.m_FirstFree);
+    m_First = Pair.first;
+    m_FirstFree = Pair.second;
+    m_Last = Pair.second;
+    cout << "Str copied." << endl;
+}
+
+Str::Str(Str &&OriStr) noexcept
+: m_First(OriStr.m_First)
+, m_FirstFree(OriStr.m_FirstFree)
+, m_Last(OriStr.m_Last)
+{
+    OriStr.m_First = nullptr;
+    OriStr.m_FirstFree = nullptr;
+    OriStr.m_Last = nullptr;
+    cout << "Str moved." << endl;
+}
+
+Str & Str::operator=(const Str &OriStr)
+{
+    auto Pair = copyMemory(OriStr.m_First, OriStr.m_FirstFree);
+
+    freeMemory();
+
+    m_First = Pair.first;
+    m_FirstFree = Pair.second;
+    m_Last = Pair.second;
+    cout << "Str assigned a value." << endl;
+
+    return *this;
+}
+
+Str::~Str()
+{
+    freeMemory();
+}
+
+void Str::pushBack(const char &NewChar)
+{
+    checkMemory();
+    m_Allocator.construct(m_FirstFree, NewChar);
+    m_FirstFree++;
+}
+
+char &Str::at(size_t Pos)
+{
+    if(Pos < size())
+    {
+        return *(m_First+Pos);
+    }
+    else
+    {
+        throw range_error("Try to refer to a position beyond range.");
+    }
+}
+
+const char *Str::c_str()
+{
+    auto Pair = copyMemory(m_First, m_FirstFree);
+    if((Pair.first != nullptr) && (Pair.second != nullptr))
+    {
+        if((Pair.first == Pair.second) || (*(Pair.second-1) != '\0'))
+        {
+            m_Allocator.construct(Pair.second, '\0');
+        }
+        else
+        {
+            //do nothing
+        }
+        return Pair.first;
+    }
+    else
+    {
+        throw runtime_error("Refered before initial.");
+    }
+}
+
+void Str::checkMemory()
+{
+    if(size() == capacity())
+    {
+        reallocateMemory();
+    }
+    else
+    {
+        //do nothing
+    }
+}
+
+void Str::reallocateMemory()
+{
+    size_t NewCapacity;
+    if(capacity() != 0)
+    {
+        NewCapacity = 2*capacity();
+    }
+    else
+    {
+        NewCapacity = 1;
+    }
+
+    reserveMemory(NewCapacity);
+}
+
+void Str::reserveMemory(size_t NewCapacity)
+{
+    if(NewCapacity > capacity())
+    {
+        char *NewFirst = m_Allocator.allocate(NewCapacity);
+        char *NewPtr = NewFirst;
+        char *OriPtr = m_First;
+        for(size_t i = 0; i != size(); i++)
+        {
+            m_Allocator.construct(NewPtr, move(*OriPtr));
+            NewPtr++;
+            OriPtr++;
+        }
+
+        freeMemory();
+        m_First = NewFirst;
+        m_FirstFree = NewPtr;
+        m_Last = NewFirst+NewCapacity;
+    }
+    else
+    {
+        //do nothing
+    }
+}
+
+pair<char *, char *> Str::copyMemory(const char *OriBegin, const char *OriEnd)
+{
+    pair<char *, char *> Pair = { nullptr, nullptr };
+    if((OriBegin != nullptr) && (OriEnd != nullptr) && (OriBegin <= OriEnd))
+    {
+        size_t Size = OriEnd-OriBegin;
+        char *NewBegin = m_Allocator.allocate(Size);
+        /*
+        char *NewPtr = NewBegin;
+        char *OriPtr = OriBegin;
+        for(size_t i = 0; i != Size; i++)
+        {
+            m_Allocator.construct(NewPtr, *OriPtr);
+            NewPtr++;
+            OriPtr++;
+        }
+        */
+        uninitialized_copy(OriBegin, OriEnd, NewBegin);
+        Pair.first = NewBegin;
+        Pair.second = NewBegin+Size;
+    }
+    else
+    {
+        //do nothing
+    }
+    return Pair;
+}
+
+void Str::freeMemory()
+{
+    if((m_First != nullptr) && (m_FirstFree != nullptr) && (m_First <= m_FirstFree))
+    {
+        for(char *Ptr = m_First; Ptr != m_FirstFree; Ptr++)
+        {
+            m_Allocator.destroy(Ptr);
+        }
+        m_Allocator.deallocate(m_First, capacity());
+        m_First = nullptr;
+        m_FirstFree = nullptr;
+        m_Last = nullptr;
+    }
+    else
+    {
+        m_First = nullptr;
+        m_FirstFree = nullptr;
+        m_Last = nullptr;
+    }
+}
+
