156cad6da85a066710bf993cc378afe55e0dbc5b
joook <outlook.s@163.com>
2021-06-15 07:16:59 +0800

    Modify answer for exercise 16.41.

--- 16.41/add.h
+++ 16.41/add.h
@@ -7,8 +7,8 @@ T add_without_expand(const T& num1, const T& num2)
     return (num1 + num2);
 }
 
-template <typename T>
-auto add_with_expand(const T& num1, const T& num2) -> decltype(num1 + num2)
+template <typename T, typename U>
+auto add_with_expand(const T& num1, const U& num2) -> decltype(num1 + num2)
 {
     return (num1 + num2);
 }
--- 16.41/main.cpp
+++ 16.41/main.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char* argv[])
 {
     auto res1 = add_without_expand(std::uint16_t(65530), std::uint16_t(6));
-    auto res2 = add_with_expand(std::uint16_t(65530), std::uint16_t(6));
+    auto res2 = add_with_expand(std::uint16_t(65530), std::uint8_t(6));
 
     std::cout << "Result of add_without_expand: " << res1 << std::endl;
     std::cout << "Result of add_with_expand: " << res2 << std::endl;
