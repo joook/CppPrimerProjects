faba6787b98b12b516aa319e4e91f64feda68516
joook <outlook.s@163.com>
2021-06-14 22:13:20 +0800

    Add answer for exercise 16.41.

---
+++ 16.41/add.h
@@ -0,0 +1,16 @@
+#ifndef _ADD_H_
+#define _ADD_H_
+
+template <typename T>
+T add_without_expand(const T& num1, const T& num2)
+{
+    return (num1 + num2);
+}
+
+template <typename T>
+auto add_with_expand(const T& num1, const T& num2) -> decltype(num1 + num2)
+{
+    return (num1 + num2);
+}
+
+#endif
