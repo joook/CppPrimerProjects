1dab6f97e13c29112db7ad33b658b01689124f1a
joook <outlook.s@163.com>
2021-06-19 10:28:27 +0800

    Modify answer for exercise 16.47.

--- 16.47/main.cpp
+++ 16.47/main.cpp
@@ -47,6 +47,10 @@ void func6(const int&& para1, int para2)
     std::cout << ", after minus: " << --para2 << std::endl;
 }
 
+void printFlip()
+{
+}
+
 int main(int argc, char* argv[])
 {
     const int arg1 = 1;
@@ -55,23 +59,39 @@ int main(int argc, char* argv[])
     const int& arg4 = arg2;
 
     // test using arg1 & arg2
-    flip(func1, arg1, arg2);
-    flip(func2, arg1, arg2);
-    flip(func3, arg1, arg2);
-    flip(func4, arg1, arg2);
-    flip(func5, arg1, 42);
-    flip(func6, arg1, 42);
+    printFlip(func1, arg1, arg2);
+    printFlip(func2, arg1, arg2);
+    printFlip(func3, arg1, arg2);
+    printFlip(func4, arg1, arg2);
+    printFlip(func5, arg1, 42);
+    printFlip(func6, arg1, 42);
 
     // test using arg1 & arg3
-    flip(func1, arg1, arg3);
-    flip(func2, arg1, arg3);
-    flip(func3, arg1, arg3);
-    flip(func4, arg1, arg3);
+    printFlip(func1, arg1, arg3);
+    printFlip(func2, arg1, arg3);
+    printFlip(func3, arg1, arg3);
+    printFlip(func4, arg1, arg3);
 
     // test using arg1 & arg4
-    flip(func1, arg1, arg4);
-    flip(func2, arg1, arg4);
-    flip(func4, arg1, arg4);
+    printFlip(func1, arg1, arg4);
+    printFlip(func2, arg1, arg4);
+    //printFlip(func3, arg1, arg4); // cannot compile
+    printFlip(func4, arg1, arg4);
+
+    // test flipWithFlaw1
+    printFlipWithFlaw1(func3, arg1, arg2); // calculate error
+    //printFlipWithFlaw1(func5, arg1, arg2); // cannot compile
+    //printFlipWithFlaw1(func6, arg1, arg2); // cannot compile
+
+    // test flipWithFlaw2
+    printFlipWithFlaw2(func3, arg1, arg2); // calculate right
+    //printFlipWithFlaw2(func5, arg1, arg2); // cannot compile
+    //printFlipWithFlaw2(func6, arg1, arg2); // cannot compile
+
+    // test flipWithFlaw3
+    printFlipWithFlaw3(func3, arg1, arg2); // calculate right
+    //printFlipWithFlaw3(func5, arg1, arg2); // cannot compile
+    //printFlipWithFlaw3(func6, arg1, arg2); // cannot compile
 
     return 0;
 }
